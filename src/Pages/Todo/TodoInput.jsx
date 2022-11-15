ccaa16c3a25c780f483b4de9df563aea3519414f
Luka <nozadzeluka2002@gmail.com>
2022-11-15 16:17:37 +0400

    fix: useEffect warning

--- src/Pages/Todo/TodoInput.jsx
+++ src/Pages/Todo/TodoInput.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 const TodoInput = (props) => {
   const [currentItem, setCurrentItem] = useState("");
   const addBtnClickHandler = (e) => {
